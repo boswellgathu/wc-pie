ae61b4f28a7c1dd28b10c50923d27068d457d5ce
boswellgathu <boswellgathu@gmail.com>
2023-12-31 07:11:35 +0300

    more readability changes

--- tests/wc_test.py
+++ tests/wc_test.py
@@ -1,13 +1,17 @@
 import subprocess
 import pytest
 
-@pytest.mark.parametrize("option,value",
-        [('-c', '342190'),
+@pytest.mark.parametrize(
+    "option, expected_value",
+    [
+        ('-c', '342190'),
         ('-l', '7145'),
-        ('-w', '58164')])
-def test_wc_option_returns_expected_value(option, value):
+        ('-w', '58164')
+    ]
+)
+def test_wc_option_returns_expected_value(option, expected_value):
     command = f"python wc.py {option} test.txt"
-    expected_output = f'{value}\ttest.txt'
+    expected_output = f'{expected_value}\ttest.txt'
 
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, _ = process.communicate()
@@ -23,11 +27,11 @@ def test_wc_no_options_returns_default():
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, _ = process.communicate()
     actual_output = output.decode().strip()
-    
+
     assert process.returncode == 0
     assert actual_output == expected_output
 
-def test_wc_can_read_from_stdin():
+def test_wc_can_read_from_stdin_when_file_is_not_provided():
     echo_command = subprocess.Popen(['echo', 'githeri'], stdout=subprocess.PIPE)
     command = "python wc.py"
     expected_output = '1\t1\t8'
@@ -48,7 +52,7 @@ def test_wc_error_handling(file_path, expected_error):
     command = f"python wc.py {file_path}"
 
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
+    _, stderr = process.communicate()
 
     assert process.returncode != 0
     assert expected_error in stderr
